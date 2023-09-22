13392eca3eff6c6ca6b3ce553e70543dfd2c197c
somuchmoh <145038131+somuchmoh@users.noreply.github.com>
2023-09-22 16:09:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Built-in functions on Python
+print("hello")
+num_char = len("hello")
+print(num_char)
+
+#Defining a function in python
+def my_function():
+  print("Hello")
+  print("Bye")
+
+my_function()    #By the time the line of codes comes to this line, it will search for the function and execute it. This is called Calling Functions 
+
+a = 2 
+if a < 3:
+    print("a = 2")  #indentation is very important in Python. Use 4 spaces to indent.
+
+
+#While Loops
+number = 6
+while number > 0:
+    number -= 1
+    print(number)
+
+#It will keep going until the statement no longer holds true 
